121a8c6ace8a195fc68ca3cc74f3670b012a43f5
Eddie Arevalo <eddiesarevalo@gmail.com>
2021-11-08 13:38:15 -0800

    Add Editable Post Deployment Config File #63
    -Added a config file that can be changed by an administrator post build and deployment.
    -The config file will be packaged with the build files. This can be changed with a normal text editor to point to a different API at any point post deployment.

---
+++ public/config.js
@@ -0,0 +1,11 @@
+window['getGlobalConfig'] = {
+    api:{
+        reserveUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/dois',
+        getDoiByLidvidUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/dois/',
+        getDoiByDoiUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/dois',
+        getDoiByPds4LabelUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/dois',
+        getDoiByIdentifier: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/doi',
+        releaseDoiUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/doi/',
+        searchUrl: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/dois'
+    } 
+}
