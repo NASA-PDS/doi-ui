403328f33589a0b62a1e49c141acd4e5a9019bd5
Eddie Arevalo <eddiesarevalo@gmail.com>
2021-11-11 17:09:32 -0800

    Merge API Endpoints Into Single Base Url #102 (#103)
    -Removed the duplication of the endpoints and used only one single base URL.
    -Removed unused function in saga and action.

--- src/Config.js
+++ src/Config.js
@@ -1,13 +1,5 @@
 const Config = {
-    api:{
-        reserveUrl: window['getGlobalConfig'].api.reserveUrl,
-        getDoiByLidvidUrl: window['getGlobalConfig'].api.getDoiByLidvidUrl,
-        getDoiByDoiUrl: window['getGlobalConfig'].api.getDoiByDoiUrl,
-        getDoiByPds4LabelUrl: window['getGlobalConfig'].api.getDoiByPds4LabelUrl,
-        getDoiByIdentifier: window['getGlobalConfig'].api.getDoiByIdentifier,
-        releaseDoiUrl: window['getGlobalConfig'].api.releaseDoiUrl,
-        searchUrl: window['getGlobalConfig'].api.searchUrl
-    } 
+    api: window['getGlobalConfig'].api
 }
 
 export default Config;
