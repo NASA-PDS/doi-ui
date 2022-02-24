4c93a27f0f07ef4d8bbf2df4d05766b88cbda9f6
Eddie Arevalo <eddiesarevalo@gmail.com>
2022-02-24 13:51:48 -0800

    Fix Doi Search Integration Bugs #117
    -Updated the doi ui to utilize the fixes in pds-wds-react.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
 import {DoiSearch} from "@nasapds/pds-wds-react";
+import { useHistory, useParams } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,10 +14,18 @@ const useStyles = makeStyles((theme) => ({
 
 const Search = (props) => {
   const classes = useStyles();
-  
+  let params = useParams();
+  const history = useHistory();
+
   return (
     <div className={classes.root}>
-      <DoiSearch showActions={props.showActions} store={props.store}/>
+      <DoiSearch 
+        useClientRouter={true} 
+        params={params}
+        history={history}
+        showActions={props.showActions} 
+        store={props.store}
+      />
     </div>
     
   )
