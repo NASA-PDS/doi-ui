ac6b6af96f93192dc8d12cc5e550bc4db9a5ffd6
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2023-05-30 14:55:38 -0700

    upgrade for stable release

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window['getGlobalConfig'] = {
-    api: 'http://localhost:8080/PDS_APIs/pds_doi_api/0.2/',
-    oauth_client_id: '<CLIENT ID OF COGNITO APP CLIENT>',
+    api: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/',
+    oauth_client_id: '1nhe6l1855mqol129tlim6g2o1',
     oauth_redirect_uri: 'http://localhost:3000',
-    oauth_logout_endpoint: 'https://<COGNITO DOMAIN NAME>.auth.us-west-2.amazoncognito.com/logout',
-    oauth_provider_url: 'https://<COGNITO DOMAIN NAME>.auth.us-west-2.amazoncognito.com/oauth2',
+    oauth_logout_endpoint: 'https://pds-doi.auth.us-west-2.amazoncognito.com/logout',
+    oauth_provider_url: 'https://pds-doi.auth.us-west-2.amazoncognito.com/oauth2',
     app_viewer_group_name: 'PDS_Viewer',
     app_admin_group_name: 'PDS_Admin',
-    pds_admin_email: '<PDS ADMIN EMAIL>'
+    pds_admin_email: 'pds-operator@jpl.nasa.gov'
 }
