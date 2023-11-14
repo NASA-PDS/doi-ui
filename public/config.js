af70d8aa0b88ca3177b33e9dddf4bde21cfc0a6b
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2023-11-14 15:56:39 -0500

    upgrade dependency for wds-react-legacy

--- public/config.js
+++ public/config.js
@@ -1,10 +1,9 @@
 window['getGlobalConfig'] = {
-    api: 'http://localhost:8085/PDS_APIs/pds_doi_api/0.2/',
+    api: 'http://localhost:8080/PDS_APIs/pds_doi_api/0.2/',
     oauth_client_id: '1nhe6l1855mqol129tlim6g2o1',
     oauth_redirect_uri: 'http://localhost:3000',
     oauth_logout_endpoint: 'https://pds-doi.auth.us-west-2.amazoncognito.com/logout',
     oauth_provider_url: 'https://pds-doi.auth.us-west-2.amazoncognito.com/oauth2',
     app_viewer_group_name: 'PDS_Viewer',
     app_admin_group_name: 'PDS_Admin',
-    pds_admin_email: 'pds-operator@jpl.nasa.gov'
 }
